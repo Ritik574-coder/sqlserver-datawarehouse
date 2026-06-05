23ed7ee2f45a5f0685ba4abaf4405d291eb9b875
ritik574-coder <ritik74820@gmail.com>
2026-06-05 08:09:39 +0530

    data qulity check and validation in gold.fact_sales
    Co-authored-by: ritsky-project <ritsky598@gmail.com>

--- Script/Analysis/dashboard_query.sql
+++ Script/Analysis/dashboard_query.sql
@@ -1,15 +1,14 @@
-SELECT TOP 10 * FROM Gold.dim_customers ; 
-SELECT TOP 10 * FROM Gold.dim_products ; 
-SELECT TOP 10 * FROM Gold.fact_sales ; 
-
 SELECT 
-c.customer_id,
-s.sales_sk,
-P.product_key ,
-s.unit_price 
-FROM Gold.dim_customers as c 
-LEFT JOIN Gold.fact_sales as s
-ON C.customer_id = s.customer_id
-LEFT JOIN Gold.dim_products as p  
-ON p.product_key = s.product_key 
-; 
+    YEAR(order_date) AS year,
+    MONTH(order_date) AS month,
+    SUM(sales_amount) AS monthly_sales
+FROM Gold.fact_sales
+GROUP BY 
+    YEAR(order_date),
+    MONTH(order_date)
+ORDER BY year, month;
+
+SELECT
+    sales_sk
+FROM Gold.fact_sales  
+WHERE sales_sk IS NULL ;
